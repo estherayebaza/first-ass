f7ff4ac60ae4985d4c2e837d3fcd19c6ffea3abd
esther ayebaza <estherayebaza@gmail.com>
2021-03-24 16:21:06 +0300

    first commit

---
+++ src/hello mum.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val name="Esther Ayebaza"
+    println(name)
+    var age=19
+    println(age)
+    val phoneNumber="0776746882"
+    println(phoneNumber)
+    var weight=54.3
+    println(weight)
+    var citezenship=false
+    println(citezenship)
+
+}
